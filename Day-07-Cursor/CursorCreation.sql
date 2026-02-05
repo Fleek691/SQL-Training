56262b91cd4468c0b83cdf8c922d1d0d5633df5a
Avishek <avishekprasad12345@gmail.com>
2026-02-05 12:06:52 +0530

    Add CursorCreation.sql

---
+++ Day-07-Cursor/CursorCreation.sql
@@ -0,0 +1,34 @@
+﻿
+--Cursor Steps: DECLARE → OPEN → FETCH → LOOP → CLOSE → DEALLOCATE
+DECLARE @ProductId INT;
+DECLARE @ProductName VARCHAR(100);
+DECLARE @Price DECIMAL(10,2);
+
+-- 1) Declare cursor
+DECLARE curProducts CURSOR FAST_FORWARD
+FOR
+    SELECT ProductId, ProductName, Price
+    FROM dbo.Products
+    ORDER BY ProductId;
+
+-- 2) Open cursor
+OPEN curProducts;
+
+-- 3) Fetch first row
+FETCH NEXT FROM curProducts INTO @ProductId, @ProductName, @Price;
+
+-- 4) Loop until no more rows
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT 'ProductId=' + CAST(@ProductId AS VARCHAR(10))
+        + ' | Name=' + @ProductName
+        + ' | Price=' + CAST(@Price AS VARCHAR(20));
+
+    -- fetch next row
+    FETCH NEXT FROM curProducts INTO @ProductId, @ProductName, @Price;
+END
+
+-- 5) Close + Deallocate
+CLOSE curProducts;
+DEALLOCATE curProducts
+
